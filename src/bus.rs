186b86edf91ae0c0a3d404ac176dbef6286109a6
Rainux Luo <rainux@gmail.com>
2026-03-28 21:47:55 +0800

    Wire Cartridge into Bus for cartridge address space routing

--- src/bus.rs
+++ src/bus.rs
@@ -1,12 +1,20 @@
+use crate::cartridge::Cartridge;
+
 pub struct Bus {
     ram: [u8; 2048],
+    pub cartridge: Option<Cartridge>,
 }
 
 impl Bus {
     pub fn new() -> Self {
         Self {
             ram: [0; 2048],
+            cartridge: None,
         }
+    }
+
+    pub fn load_cartridge(&mut self, cartridge: Cartridge) {
+        self.cartridge = Some(cartridge);
     }
 
     pub fn read(&mut self, addr: u16) -> u8 {
@@ -17,8 +25,14 @@ impl Bus {
             0x2000..=0x3FFF => 0,
             // APU + I/O (stub)
             0x4000..=0x4017 => 0,
-            // Cartridge space (stub)
-            0x4020..=0xFFFF => 0,
+            // Cartridge space
+            0x4020..=0xFFFF => {
+                if let Some(ref cart) = self.cartridge {
+                    cart.mapper.cpu_read(addr)
+                } else {
+                    0
+                }
+            }
             _ => 0,
         }
     }
@@ -28,7 +42,11 @@ impl Bus {
             0x0000..=0x1FFF => self.ram[(addr & 0x07FF) as usize] = data,
             0x2000..=0x3FFF => {} // PPU stub
             0x4000..=0x4017 => {} // APU/IO stub
-            0x4020..=0xFFFF => {} // Cartridge stub
+            0x4020..=0xFFFF => {
+                if let Some(ref mut cart) = self.cartridge {
+                    cart.mapper.cpu_write(addr, data);
+                }
+            }
             _ => {}
         }
     }
@@ -59,5 +77,23 @@ mod tests {
         let mut bus = Bus::new();
         bus.write(0x0800, 0xCD);
         assert_eq!(bus.read(0x0000), 0xCD);
+    }
+
+    #[test]
+    fn test_cartridge_read() {
+        let mut bus = Bus::new();
+        let mut prg = vec![0u8; 32768];
+        prg[0] = 0xEA; // NOP at $8000
+        prg[0x7FFC] = 0x00; // Reset vector low ($FFFC)
+        prg[0x7FFD] = 0x80; // Reset vector high -> $8000
+        let chr = vec![0u8; 8192];
+        let cart = crate::cartridge::Cartridge {
+            mapper: Box::new(crate::mapper::mapper0::Mapper0::new(prg, chr)),
+            mirroring: crate::cartridge::Mirroring::Horizontal,
+        };
+        bus.load_cartridge(cart);
+        assert_eq!(bus.read(0x8000), 0xEA);
+        assert_eq!(bus.read(0xFFFC), 0x00);
+        assert_eq!(bus.read(0xFFFD), 0x80);
     }
 }
